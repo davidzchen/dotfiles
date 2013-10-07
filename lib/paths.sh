1eb89d2789c41ebe272c78febb8099f82ad9f3f9
David Z. Chen <dchen@linkedin.com>
2013-10-07 13:31:07 -0700

    Add to paths.

--- lib/paths.sh
+++ lib/paths.sh
@@ -6,13 +6,13 @@ if [ -d $HOME/.cabal/bin ]; then
   export PATH="$HOME/.cabal/bin:$PATH"
 fi
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:/opt/lib
-export LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:/opt/lib
-export C_INCLUDE_PATH=$C_INCLUDE_PATH:/usr/local/include:/opt/include
-export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/usr/local/include:/opt/include
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:/opt/lib:$HOME/local/lib
+export LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:/opt/lib:$HOME/local/lib
+export C_INCLUDE_PATH=$C_INCLUDE_PATH:/usr/local/include:/opt/include:$HOME/local/include
+export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/usr/local/include:/opt/include:$HOME/local/include
 
-if [ -d $HOME/Projects/hadoop-common/bin ]; then
-  export PATH="$HOME/Projects/hadoop-common/bin:$PATH"
+if [ -d $HOME/Projects/hadoop/bin ]; then
+  export PATH="$HOME/Projects/hadoop/bin:$PATH"
 fi
 
 if [ -d $HOME/Projects/pig/bin ]; then
