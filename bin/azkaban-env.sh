622c54906d2aa3b7c568b021253db50fbaca3a73
David Z. Chen <david.z.chen@outlook.com>
2013-12-21 23:44:29 -0800

    OS X specific path changes.

--- bin/azkaban-env.sh
+++ bin/azkaban-env.sh
@@ -1,11 +1,11 @@
 # Path to executor server.
-export AZKABAN_EXECUTOR_HOME=/home/dzc/Projects/azkaban2/dist/packages/azkaban-exec-server
+export AZKABAN_EXECUTOR_HOME=/Users/dzc/Projects/azkaban2/dist/packages/azkaban-exec-server
 
 # Path to web server.
-export AZKABAN_WEB_HOME=/home/dzc/Projects/azkaban2/dist/packages/azkaban-web-server
+export AZKABAN_WEB_HOME=/Users/dzc/Projects/azkaban2/dist/packages/azkaban-web-server
 
 # Path to solo server.
-export AZKABAN_SOLO_HOME=/home/dzc/Projects/azkaban2/dist/packages/azkaban-solo-server
+export AZKABAN_SOLO_HOME=/Users/dzc/Projects/azkaban2/dist/packages/azkaban-solo-server
 
 # Path to admin server.
-export AZKABAN_ADMIN_HOME=/home/dzc/Projects/azkaban2/dist/packages/azkaban-admin-server
+export AZKABAN_ADMIN_HOME=/Users/dzc/Projects/azkaban2/dist/packages/azkaban-admin-server
