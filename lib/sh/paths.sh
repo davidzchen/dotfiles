a46e65b382a0955bf7a71562a41335db1e3c2920
David Z. Chen <david@davidzchen.com>
2015-09-19 22:57:59 -0700

    Update paths.sh to add Mono commands directory to PATH on OS X.

--- lib/sh/paths.sh
+++ lib/sh/paths.sh
@@ -20,6 +20,13 @@ case $(uname -s) in
     if [ -d $HOME/Library/Haskell/bin ]; then
       export PATH=$HOME/Library/Haskell/bin:$PATH
     fi
+    # If Mono is installed, put the Commands directory at the end of $PATH.
+    # Because a number of commands in the Mono distribution overlap with those
+    # installed on the system, this way, the system version of the commands
+    # will be used rather than the Mono ones.
+    if [ -d /Library/Frameworks/Mono.framework/Commands ]; then
+      export PATH=$PATH:/Library/Frameworks/Mono.framework/Commands
+    fi
     ;;
 esac
 export GOPATH=$HOME/go
