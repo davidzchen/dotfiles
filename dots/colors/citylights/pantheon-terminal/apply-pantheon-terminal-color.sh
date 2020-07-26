4ebd31e762d2086168b7039a955e11d49bd9b3e7
David Z. Chen <david@davidzchen.com>
2020-07-26 11:44:55 -0700

    Fix script for applying Pantheon Terminal color scheme.

--- dots/colors/citylights/pantheon-terminal/apply-pantheon-terminal-color.sh
+++ dots/colors/citylights/pantheon-terminal/apply-pantheon-terminal-color.sh
@@ -2,7 +2,7 @@
 
 set -euf -o pipefail
 
-readonly BLACK="#1D252C"      # black    terminal_color_0
+readonly BLACK="#1B232A"      # black    terminal_color_0
 readonly RED="#D95468"        # red      terminal_color_1
 readonly GREEN="#8BD49C"      # green    terminal_color_2
 readonly ORANGE="#D98E48"     # orange   terminal_color_3
@@ -20,11 +20,12 @@ readonly BR_MAGENTA="#B62D65" # bright magenta  terminal_color_13
 readonly BR_CYAN="#70E1E8"    # bright cyan     terminal_color_14
 readonly BR_WHITE="#B7C5D3"   # bright white    terminal_color_15
 
-readonly BACKGROUND='rgba(29,37,44,0.95)'  # $BLACK but with 95% opacity
+#readonly BACKGROUND='rgba(29,37,44,0.95)'  # $BLACK but with 95% opacity
+readonly BACKGROUND="$BLACK"
 readonly FOREGROUND="$BR_WHITE"
 readonly CURSOR="$BLUE"
 readonly DARKSTYLE='true'
-readonly PALETTE="$BLACK,$RED,$GREEN,$ORANGE,$BLUE,$MAGENTA,$CYAN,$WHITE,$BR_BLACK,$BR_RED,$BR_GREEN,$BR_ORANGE,$BR_BLUE,$BR_MAGENTA,$BR_CYAN,$BR_WHITE"
+readonly PALETTE="$BLACK:$RED:$GREEN:$ORANGE:$BLUE:$MAGENTA:$CYAN:$WHITE:$BR_BLACK:$BR_RED:$BR_GREEN:$BR_ORANGE:$BR_BLUE:$BR_MAGENTA:$BR_CYAN:$BR_WHITE"
 
 gsettings set io.elementary.terminal.settings background "$BACKGROUND"
 gsettings set io.elementary.terminal.settings foreground "$FOREGROUND"
