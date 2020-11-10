b6236a9e170e596da440f60f94fc9ee9c3abcbd4
David Z. Chen <mail@davidzchen.com>
2020-11-10 13:00:06 -0800

    WIP: Script for common setup steps.

---
+++ dots/setup/common.sh
@@ -0,0 +1,7 @@
+# Bzzel tools
+
+go get github.com/bazelbuild/buildtools/unused_deps
+go get github.com/bazelbuild/buildtools/buildifier
+go get github.com/bazelbuild/buildtools/buildozer
+go get github.com/bazelbuild/buildtools/bazelisk
+go get github.com/bazelbuild/bazel-gazelle/cmd/gazelle
